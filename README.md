b3b626c6c5034f941a51be2ebdcac817a9773942
Evan Strat <evan10s@users.noreply.github.com>
2021-07-22 23:01:38 -0400

    Added Detekt linter

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # apiary-mobile
 
 The companion mobile app for MyRoboJackets
+
+## Local development
+
+### Code linting
+
+Detekt is used for linting Kotlin code.  The recommended command to run it is
+
+_(Windows)_
+```bash
+    gradlew detektAll -PdetektAutoFix=true
+```
+
+_(*nix)_
+```bash
+    ./gradlew detektAll -PdetektAutoFix=true
+```
+
+The detektAutoFix parameter will automatically fix simple issues.
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,10 @@ allprojects {
     }
 }
 
+plugins {
+    id("io.gitlab.arturbosch.detekt").version("1.18.0-RC2")
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "1.8"
@@ -26,4 +30,34 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+val projectSource = file(projectDir)
+val configFile = files("$rootDir/config/detekt/detekt.yml")
+val baselineFile = file("$rootDir/config/detekt/baseline.xml")
+val kotlinFiles = "**/*.kt"
+val resourceFiles = "**/resources/**"
+val buildFiles = "**/build/**"
+
+tasks.register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
+    val autoFix = project.hasProperty("detektAutoFix")
+
+    description = "Custom DETEKT build for all modules"
+    parallel = true
+    ignoreFailures = false
+    autoCorrect = autoFix
+    buildUponDefaultConfig = true
+    setSource(projectSource)
+    config.setFrom(configFile)
+    include(kotlinFiles)
+    exclude(resourceFiles, buildFiles)
+    reports {
+        html.enabled = true
+        xml.enabled = false
+        txt.enabled = false
+    }
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
 }
