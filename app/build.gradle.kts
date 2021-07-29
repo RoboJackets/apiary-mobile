770ace0b71fb677500be81100defb2dabea67318
Evan Strat <evan10s@users.noreply.github.com>
2021-07-29 01:19:12 -0400

    Upgrade to stable Compose versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,35 +16,35 @@ dependencies {
 
     // Basics
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("androidx.activity:activity-compose:1.3.0")
     androidTestImplementation("junit:junit:4.13.2")
 
     // Compose
-    implementation("androidx.compose.ui:ui:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-beta08")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.12.0")
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
     // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-    implementation("androidx.compose.foundation:foundation:1.0.0-rc02")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
     // Material Design
-    implementation("androidx.compose.material:material:1.0.0-rc02")
+    implementation("androidx.compose.material:material:1.0.0")
     // Material design icons
-    implementation("androidx.compose.material:material-icons-core:1.0.0-rc02")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0-rc02")
+    implementation("androidx.compose.material:material-icons-core:1.0.0")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0")
     // Integration with observables
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-rc02")
-    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-rc02")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("androidx.activity:activity-compose:1.3.0")
 
     implementation(files("../libs/nxpnfcandroidlib-release.aar"))
     implementation("com.google.firebase:firebase-core:17.2.2") // Required when including TapLinx (line above) manually
 
     // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-rc02")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.3.5")
     // Hilt
@@ -65,7 +65,7 @@ android {
     compileSdk = 30
     defaultConfig {
         applicationId = "org.robojackets.apiary"
-        minSdk = 21
+        minSdk = 24 // FIXME: this is temporary to workaround a Compose bug (https://issuetracker.google.com/issues/194289155)
         targetSdk = 30
         versionCode = 1
         versionName = "1.0"
@@ -90,7 +90,7 @@ android {
         jvmTarget = "1.8"
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-rc02"
+        kotlinCompilerExtensionVersion = "1.0.0"
     }
     hilt {
         enableExperimentalClasspathAggregation = true
--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -18,29 +18,29 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("androidx.activity:activity-compose:1.3.0")
     androidTestImplementation("junit:junit:4.13.2")
 
     // Compose
-    implementation("androidx.compose.ui:ui:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui:1.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-beta08")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.12.0")
 
     // Tooling support (Previews, etc.)
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0")
     // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-    implementation("androidx.compose.foundation:foundation:1.0.0-rc02")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
     // Material Design
-    implementation("androidx.compose.material:material:1.0.0-rc02")
+    implementation("androidx.compose.material:material:1.0.0")
 
     // Integration with observables
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-rc02")
-    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-rc02")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("androidx.activity:activity-compose:1.3.0")
 
     // UI Tests
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0-rc02")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.0")
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
     // Hilt
@@ -84,7 +84,7 @@ android {
         jvmTarget = "1.8"
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-rc02"
+        kotlinCompilerExtensionVersion = "1.0.0"
     }
     hilt {
         enableExperimentalClasspathAggregation = true
